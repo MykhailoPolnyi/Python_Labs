a2beeb07459cc7034131e8b8a2526710c161aba2
MykhailoPolnyi <mykhailo.polnyi.knm.2020@plnu.ua>
2021-02-20 23:55:34 +0200

    Added files of completed first lab task, updeted readme file

---
+++ DiscountedProduct.py
@@ -0,0 +1,33 @@
+class DiscountedProduct:
+    __discounted_products_counter = 0
+
+    def __init__(self, /, product_name="", producer="", *, starting_price=0,
+                 discount_in_percents=0, discount_duration_days=0, customers_rate=0):
+        self.name = product_name
+        self.producer = producer
+        self.starting_price = starting_price
+        self.discount_in_percents = discount_in_percents
+        if self.starting_price != 0:
+            self.price_with_discount = starting_price * (1 - discount_in_percents / 100)
+        self.discount_duration_days = discount_duration_days
+        self.customers_rate = customers_rate
+        DiscountedProduct.__discounted_products_counter += 1
+
+    def __str__(self):
+        return f"Product name: {self.name}\n\
+Discount: {self.discount_in_percents}%\n\
+Starting Price: {self.starting_price} \n\
+Price with discount: {self.price_with_discount}\n\
+Producer: {self.producer} \n\
+Duration of discount: {self.discount_duration_days} days\n\
+Customers rate of this product: {self.customers_rate}/10\n"
+
+    def __del__(self):
+        del self
+        DiscountedProduct.__discounted_products_counter -= 1
+        print(f"Object Deleted\n\
+Objects left: {DiscountedProduct.__discounted_products_counter}")
+
+    @staticmethod
+    def get_disc_product_counter():
+        return DiscountedProduct.__discounted_products_counter
--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# Python_Labs
+# Task
+Створити консольну програму мовою Python і написати клас <Назва_із_завдання> який міститиме:
+
+1.Три додаткових поля, які найкраще описують даний клас    
+2.Конструктор (із вказаними дефолтними значеннями)
+3.Деструктор
+4.Метод, що повертає стрічкове представлення класу (__str__())
+5.Статичне поле
+6.Статичний метод, що повертає значення статичного поля
+7.В main() методі визначіть 3 об’єкти типу із завдання (з-за допомогою передачі різної кількості параметрів) та виведіть інформацію про них з-за допомогою методу з пункту 4 в консоль
+
+Створити клас “Акційний товар” котрий містить поля:
+- % знижки
+- назва товару
+- початкова ціна
+- акційна ціна
+
+
+To run the program in command line:
+1.to copy repository on your current disk/folder use "git clone <address_of_repository>.git"
+2.to open folder with local repository use "cd <repository_name>"
+3.to run the program with extension .py use "python file_name.py"
+
+In current expamle use:
+1.git clone https://github.com/MykhailoPolnyi/Python_Labs.git
+2.cd Python_Labs
+3.python main.py
